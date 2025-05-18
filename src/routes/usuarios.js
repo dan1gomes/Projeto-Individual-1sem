1dabc681d3897fdf3a4cb85f3afd0b41e055f366
dan1gomes <daniela.carvalho@sptech.school>
2025-05-17 21:42:17 -0300

    Subindo projeto com a API e começo da página de votação

---
+++ src/routes/usuarios.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var router = express.Router();
+
+var usuarioController = require("../controllers/usuarioController");
+
+//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
+// .post é para inserir e .get é para pegar os dados
+router.post("/cadastrar", function (req, res) {
+    usuarioController.cadastrar(req, res);
+})
+
+router.post("/autenticar", function (req, res) {
+    usuarioController.autenticar(req, res);
+});
+
+module.exports = router;
